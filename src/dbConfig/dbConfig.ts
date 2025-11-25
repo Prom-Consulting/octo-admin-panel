10991a9ad13d3f7c8a4fb9e1184c3f8d21efc5e7
Aybek kyzy Begaym <ajbekkyzybegajym1@gmail.com>
2025-11-25 12:03:54 +0600

    diagnostic logs added

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -6,7 +6,7 @@ export const env = cleanEnv(process.env, {
   PG_USER: str(),
   PG_PASSWORD: str(),
   PG_HOST: str({ default: "localhost" }),
-  PG_PORT: num({ default: 5432 }),
+  // PG_PORT: num({ default: 5432 }),
 
   PG_MASTER_DATABASE: str(),
   PG_MASTER_USER: str(),
@@ -21,7 +21,7 @@ export const sequelize = new Sequelize(
   env.PG_PASSWORD,
   {
     host: env.PG_HOST,
-    port: env.PG_PORT,
+    // port: env.PG_PORT,
     dialect: "postgres",
     logging: process.env.NODE_ENV === "development",
     dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
